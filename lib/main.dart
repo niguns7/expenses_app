2a3675106c95881aed8fc0cef753656756c9acea
niguns7 <nirja767@gmail.com>
2022-12-06 15:18:12 +0545

    string interpolation

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class MyHomePage extends StatelessWidget {
                         border: Border.all(color: Colors.purple, width: 2),
                       ),
                       child: Text(
-                        tx.amount.toString(),
+                        '\$${tx.amount}', //string interpolation
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
