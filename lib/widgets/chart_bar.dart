b2ccfbe31cf2d7a347e5f6c89524bb7d460e3bff
niguns7 <nirja767@gmail.com>
2022-12-13 14:39:55 +0545

    wrapped with new widgits

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -5,39 +5,45 @@ class ChartBar extends StatelessWidget {
   final double SpendingAmount;
   final double SpendingPofTotal;
 
-  ChartBar(this.lable,this.SpendingAmount,this.SpendingPofTotal);
+  ChartBar(this.lable, this.SpendingAmount, this.SpendingPofTotal);
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Text('\$${SpendingAmount.toStringAsFixed(2)}'),
-      SizedBox(height: 9,),
-      Container(
-        height: 90,
-        width: 10,
-        child: Stack(children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey, width: 1),
-              color: Color.fromRGBO(220, 220, 220, 1),
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          FractionallySizedBox(
-            heightFactor: SpendingPofTotal,
-            child: 
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 20,
+            child: FittedBox(
+                child: Text('\$${SpendingAmount.toStringAsFixed(0)}'))),
+        SizedBox(
+          height: 9,
+        ),
+        Container(
+          height: 90,
+          width: 10,
+          child: Stack(children: <Widget>[
             Container(
               decoration: BoxDecoration(
-                color: Colors.purple,
+                border: Border.all(color: Colors.grey, width: 1),
+                color: Color.fromRGBO(220, 220, 220, 1),
                 borderRadius: BorderRadius.circular(10),
-                ),),
-          )
-        ]),
-      ),
-      SizedBox(height: 10,),
-
-    ],);
+              ),
+            ),
+            FractionallySizedBox(
+              heightFactor: SpendingPofTotal,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            )
+          ]),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+      ],
+    );
   }
 }
-
-
