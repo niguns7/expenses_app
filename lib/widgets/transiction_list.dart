e82a912bb527b3cb0e279787e809c218c731b890
niguns7 <nirja767@gmail.com>
2022-12-13 14:40:39 +0545

    ui of the transaction list changed

--- lib/widgets/transiction_list.dart
+++ lib/widgets/transiction_list.dart
@@ -14,51 +14,35 @@ class TransactionList extends StatelessWidget {
           ? Column(
               children: <Widget>[
                 Container(
-                  height: 520,
-                  width: 600,
+                    height: 520,
+                    width: 600,
                     child: Image.asset(
-                  'assets/images/image.png',
-                  fit: BoxFit.cover,
-                ))
+                      'assets/images/image.png',
+                      fit: BoxFit.cover,
+                    ))
               ],
             )
           : ListView.builder(
               itemBuilder: (context, index) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Theme.of(context).primaryColor, width: 2),
-                        ),
-                        child: Text(
-                          '\$${transaction[index].amount.toStringAsFixed(2)}', //string interpolation
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColor),
-                        ),
-                        padding: EdgeInsets.all(10),
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8,horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                            child: Text('\$${transaction[index].amount}')),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transaction[index].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(transaction[index].date),
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontWeight: FontWeight.w700),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      transaction[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle:Text(
+                      DateFormat.yMMMd().format(transaction[index].date) ,
+                    ) ,
                   ),
                 );
               },
