941a94c0c8ee1f167fce4b936d10e75df9de94f2
niguns7 <nirja767@gmail.com>
2022-12-15 14:06:17 +0545

    setted the preffered orentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './widgets/chart.dart';
 import './widgets/transiction_list.dart';
 import './widgets/new_transaction.dart';
 import './modal/transaction.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); //forces our app to stay on potrait mode even the device rotates
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,21 +18,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Expenses App',
       theme: ThemeData(
-          primarySwatch: Colors.purple, //sets defult color
-          fontFamily: 'QuickSand',
-          textTheme: ThemeData.light().textTheme.copyWith(
+        primarySwatch: Colors.purple, //sets defult color
+        fontFamily: 'QuickSand',
+        textTheme: ThemeData.light().textTheme.copyWith(
             headline6: TextStyle(
-              fontFamily: 'Quicksand',
-              fontWeight: FontWeight.bold, 
-              fontSize: 18),
-              button: TextStyle(color: Colors.white)
-          ),
-          appBarTheme: AppBarTheme(
-              textTheme: ThemeData.light().textTheme.copyWith(
-                  headline6: TextStyle(
+                fontFamily: 'Quicksand',
+                fontWeight: FontWeight.bold,
+                fontSize: 18),
+            button: TextStyle(color: Colors.white)),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
                     fontFamily: 'OpenSans',
-                    fontSize: 20 ,
-                    fontWeight: FontWeight.bold),),),),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+        ),
+      ),
       home: MyHomePage(),
     );
   }
@@ -56,17 +64,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<Transaction> get _recentTransaction {
     return _userTransactions.where((tx) {
-      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7),),);
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
     }).toList();
   }
 
   void _addTransaction(String txTitle, double txAmount, DateTime choosenDate) {
     final newTx = Transaction(
-        id: DateTime.now().toString(),
-        title: txTitle,
-        amount: txAmount,
-        date: choosenDate,
-        );
+      id: DateTime.now().toString(),
+      title: txTitle,
+      amount: txAmount,
+      date: choosenDate,
+    );
 
     setState(() {
       _userTransactions.add(newTx);
@@ -86,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _deletTransaction (String id) {
+  void _deletTransaction(String id) {
     setState(() {
       _userTransactions.removeWhere((tx) {
         return tx.id == id;
@@ -97,17 +109,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final appBar = AppBar(
-        title: Text(
-          'Expenses App',
-          style: TextStyle(fontFamily: 'OpenSans'),
+      title: Text(
+        'Expenses App',
+        style: TextStyle(fontFamily: 'OpenSans'),
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _StartNewtransaction(context),
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _StartNewtransaction(context),
-          ),
-        ],
-      );
+      ],
+    );
     return Scaffold(
       appBar: appBar,
       body: SingleChildScrollView(
@@ -115,11 +127,15 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
-              height: (MediaQuery.of(context).size.height) *0.35 - appBar.preferredSize.height-MediaQuery.of(context).padding.top,
-              child: Chart(_recentTransaction)),
+                height: (MediaQuery.of(context).size.height) * 0.35 -
+                    appBar.preferredSize.height -
+                    MediaQuery.of(context).padding.top,
+                child: Chart(_recentTransaction)),
             Container(
-              height: (MediaQuery.of(context).size.height) *0.7 - appBar.preferredSize.height-MediaQuery.of(context).padding.top,
-              child: TransactionList(_userTransactions, _deletTransaction)),
+                height: (MediaQuery.of(context).size.height) * 0.7 -
+                    appBar.preferredSize.height -
+                    MediaQuery.of(context).padding.top,
+                child: TransactionList(_userTransactions, _deletTransaction)),
           ],
         ),
       ),
